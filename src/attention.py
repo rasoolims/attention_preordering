f38a9800f952a1d3a48017476c611d13a30c2a61
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2018-03-22 11:07:43 -0400

    remove char cnn temporarily

--- src/attention.py
+++ src/attention.py
@@ -73,15 +73,16 @@ class MT:
         self.generate_emb_mask = _emb_mask_generator
 
     def embed_sentence(self, ws, pwords, ts, chars, is_train):
-        cembed = [dy.lookup_batch(self.clookup, c) for c in chars]
-        char_fwd, char_bckd = self.char_lstm.builder_layers[0][0].initial_state().transduce(cembed)[-1], \
-                              self.char_lstm.builder_layers[0][1].initial_state().transduce(reversed(cembed))[-1]
-        crnn = dy.reshape(dy.concatenate_cols([char_fwd, char_bckd]), (self.options.we, ws.shape[0] * ws.shape[1]))
-        cnn_reps = [list() for _ in range(len(ws))]
-        for i in range(ws.shape[0]):
-            cnn_reps[i] = dy.pick_batch(crnn, [i * ws.shape[1] + j for j in range(ws.shape[1])], 1)
-
-        wembed = [dy.lookup_batch(self.wlookup, ws[i]) + dy.lookup_batch(self.elookup, pwords[i]) + cnn_reps[i] for i in range(len(ws))]
+        # cembed = [dy.lookup_batch(self.clookup, c) for c in chars]
+        # char_fwd, char_bckd = self.char_lstm.builder_layers[0][0].initial_state().transduce(cembed)[-1], \
+        #                       self.char_lstm.builder_layers[0][1].initial_state().transduce(reversed(cembed))[-1]
+        # crnn = dy.reshape(dy.concatenate_cols([char_fwd, char_bckd]), (self.options.we, ws.shape[0] * ws.shape[1]))
+        # cnn_reps = [list() for _ in range(len(ws))]
+        # for i in range(ws.shape[0]):
+        #     cnn_reps[i] = dy.pick_batch(crnn, [i * ws.shape[1] + j for j in range(ws.shape[1])], 1)
+        #
+        # wembed = [dy.lookup_batch(self.wlookup, ws[i]) + dy.lookup_batch(self.elookup, pwords[i]) + cnn_reps[i] for i in range(len(ws))]
+        wembed = [dy.lookup_batch(self.wlookup, ws[i]) + dy.lookup_batch(self.elookup, pwords[i]) for i in range(len(ws))]
         posembed = [dy.lookup_batch(self.tlookup, ts[i]) for i in range(len(ts))]
         if (not is_train) or self.options.dropout == 0:
             return [dy.concatenate([wembed[i], posembed[i]]) for i in range(len(ts))]
