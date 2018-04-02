f74b9eae07abab06e6eeed9c2dde74089325fe01
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2018-04-02 15:05:45 -0400

    debugging

--- src/attention.py
+++ src/attention.py
@@ -361,8 +361,10 @@ class MT:
                     new_beam_elem = BeamElement(s, new_mask, new_out, last_output_embeddings, last_tag_embeddings,
                                                 beam_elem.score + next_score)
                     current_beam.append(new_beam_elem)
+            print [b.score for b in current_beam]
             beamElements = sorted(current_beam)[:beam_size]
-
+            print [b.score for b in beamElements]
+            print '*****'
         out_val = sorted(current_beam)[0].out
         dy.renew_cg()
         return out_val
